670641e16d7bc988b754cd2eda6bfdd8150bb9f5
Rockmafia <67574563+Rockmafia88@users.noreply.github.com>
2023-09-06 05:46:28 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison_of_datatype.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/* พยายามหลีกเลี่ยง Code เหล่านี้ */
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //โค๊ดเหล่านี้ทำให้เราสับสนได้ในอนาคต
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0); // Compaison กับ == ทำงานต่างกัน
+
+
+
+// === 
+
+console.log("2" === 2);
+ 
