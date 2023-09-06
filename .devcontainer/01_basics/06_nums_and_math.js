3d6b8316779469d57d0b980a53b53748ed375922
Rockmafia <67574563+Rockmafia88@users.noreply.github.com>
2023-09-06 10:12:28 +0000

    เพิ่มคอมเม้น

--- .devcontainer/01_basics/06_nums_and_math.js
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -32,7 +32,7 @@ console.log(Math.floor(Math.random()*10) + 1);
 const min = 10 
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // 20 - 10 = + 1 = 11 + 10 = 21
 
 
 
