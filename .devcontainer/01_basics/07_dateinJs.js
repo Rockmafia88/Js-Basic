6d92701708c22abbe513d5a9723a29253f10d8b2
Rockmafia <67574563+Rockmafia88@users.noreply.github.com>
2023-09-07 04:30:37 +0000

    Datetime

---
+++ .devcontainer/01_basics/07_dateinJs.js
@@ -0,0 +1,37 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate); //เป็น Object
+
+let myCreateDate = new Date(2023, 0, 23)
+let myCreateDateOne = new Date(2023, 1, 23)
+let myCreateDateTwo = new Date("2023-01-14") //yyyy/mm/day
+// console.log(myCreateDate.toDateString());
+console.log(myCreateDateTwo);
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreateDateTwo.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+ 1);
+console.log(newDate.getDate());
+
+
+`${newDate.getDay()} and the time is ${newDate.getTime()}`  
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+
+})
+
+console.log(newDate);
